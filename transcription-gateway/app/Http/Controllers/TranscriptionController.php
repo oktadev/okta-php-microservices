088106d8c2a01c0d76e6ca61d35c4a23135bb5aa
Krasimir Hristozov <krasi.hristozov@gmail.com>
2019-05-13 01:36:30 +0300

    Added putting a message on the queue in the transcription gateway

--- transcription-gateway/app/Http/Controllers/TranscriptionController.php
+++ transcription-gateway/app/Http/Controllers/TranscriptionController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Aws\Sqs\SqsClient;
 
 class TranscriptionController extends Controller
 {
@@ -18,6 +19,31 @@ class TranscriptionController extends Controller
             'user-audio-file-url' => $request->input('audio-file-url')
         ];
 
+        // of course, this should be extracted to a service
+        // instead of using a private method on the controller:
+        $this->putMessageOnQueue($message);
+
         return response()->json($message, 201);
+    }
+
+    private function putMessageOnQueue($message)
+    {
+        $key = getenv('AWS_ACCESS_KEY_ID');
+        $secret = getenv('AWS_SECRET_ACCESS_KEY');
+
+        $client = SqsClient::factory([
+            'key' => $key,
+            'secret' => $secret,
+            'version' => '2012-11-05',
+            // modify the region if necessary:
+            'region'  => 'us-east-1',
+        ]);
+
+        $result = $client->sendMessage(array(
+            'QueueUrl'    => getenv('AWS_QUEUE_URL_TRANSCRIBE'),
+            'MessageBody' => json_encode($message)
+        ));
+
+        return $result;
     }
 }
